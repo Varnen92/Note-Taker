eac13230a29b4552434182a42bcd62bc17410c93
Robert Sandoval <robsand1992@yahoo.com>
2021-11-28 11:11:34 -0800

    updated file trees

--- server.js
+++ server.js
@@ -10,9 +10,17 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
+app.get('./notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
-
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`API Server now on port ${POST}!`)
