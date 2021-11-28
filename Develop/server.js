f97b34fead65596c59809cbd457e557f14a16f07
Robert Sandoval <robsand1992@yahoo.com>
2021-11-28 11:03:06 -0800

    Add Heroku

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const fs = require ('fs')
+const path = require('path')
+const express = require('express')
+const { notes } = require('./db/db.json')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API Server now on port ${POST}!`)
+})
